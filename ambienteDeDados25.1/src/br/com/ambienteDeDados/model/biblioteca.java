339ddef8c0f0da85a8111ee94fa4477c527ff168
Unifor <franciscofernandes@unifor.br>
2025-05-29 17:00:43 -0300

    iniciando atividade

---
+++ ambienteDeDados25.1/src/br/com/ambienteDeDados/model/biblioteca.java
@@ -0,0 +1,36 @@
+package br.com.ambienteDeDados.model;
+
+import java.util.Date;
+public class biblioteca {
+	
+	private int id;
+	private String name;
+	private int idade;
+	private Date dataCadastro;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public Date getDataCadastro() {
+		return dataCadastro;
+	}
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+	
+}
